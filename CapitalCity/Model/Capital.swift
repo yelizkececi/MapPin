a9b8c524cf7e9d0a9b077f3f1e2d413769dfb0f4
Yeliz Kececi <yelizkececi>
2021-01-22 15:56:06 +0300

    Add: CapitalModel Add: MapView

---
+++ CapitalCity/Model/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  CapitalCity
+//
+//  Created by Yeliz Keçeci on 21.01.2021.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
